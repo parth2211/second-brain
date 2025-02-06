ce42164bd355108783852b736c35ca45d7ee81d6
parth2211 <parthdhorajiya365@gmail.com>
2025-02-06 23:04:12 +0530

    initialsed User Schema

--- src/db.ts
+++ src/db.ts
@@ -1,2 +1,12 @@
 //Create user models and schema
 
+import mongoose, {model, Schema} from 'mongoose';
+
+mongoose.connect("url")
+
+const UserSchema = new Schema({
+    username: {type: String, unique: true},
+    password: String
+})
+
+export const UserModel = model("User", UserSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
+import { UserModel } from './db';
 
 const app = express();
+app.use(express.json());
 
-app.post("/api/v1/signin", (req, res) => {
+app.post("/api/v1/signup", async (req, res) => {
+    //zod validation, hash the password
+    const username = req.body.username;
+    const password = req.body.password;
 
+    await UserModel.create({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        message: "User signed up"
+    })
 })
 
 app.post("/api/v1/content", (req, res) => {

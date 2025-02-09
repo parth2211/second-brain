b95caf2074dbe95ab91744e762d6055c1b3f851f
parth2211 <parthdhorajiya365@gmail.com>
2025-02-09 19:20:53 +0530

    impl share api endpoint

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
-import { ContentModel, UserModel } from './db';
+import { ContentModel, LinkModel, UserModel } from './db';
 import { JWT_PASSWORD } from './config';
 import { userMiddleware } from './middleware';
+import { random } from './utils';
 
 const app = express();
 app.use(express.json());
@@ -91,12 +92,71 @@ app.delete("/api/v1/content", userMiddleware, async (req, res) => {
     })
 })
 
-app.post("/api/v1/brain/share", (req, res) => {
-    
+app.post("/api/v1/brain/share", userMiddleware, async (req, res) => {
+    const share = req.body.share;
+    if(share) {
+
+        try {
+            const hash = random(10);
+            await LinkModel.create({
+                userId: req.body.userId,
+                hash: hash
+            }) 
+
+            res.json({
+                message: "link/" + hash
+            })
+
+        } catch(e) {
+            res.json({
+                messgae: "Share Link already exist"
+            })
+        }
+        
+    } else {
+        await LinkModel.deleteOne({
+            userId: req.body.userId
+        })
+
+        res.json({
+            message: "Link removed"
+        })
+    }
 })
 
-app.get("/api/v1/brain/:shareLink", (req, res) => {
+app.get("/api/v1/brain/:shareLink", async (req, res) => {
+    const hash = req.params.shareLink
     
+    const link = await LinkModel.findOne({
+        hash: hash,
+    })
+
+    if(!link) {
+        res.status(411).json({
+            message: "Inc input"
+        })
+    } else {
+        const content = await ContentModel.find({
+            userId: link.userId
+        })
+
+        const user = await UserModel.findOne({
+            _id: link.userId
+        })
+
+        if(!user) {
+            res.status(411).json({
+                message: "User not found"
+            })
+            return;
+        }
+
+        res.json({
+            username: user.username,
+            content: content
+        })
+    }    
+
 })
 
 app.listen(3000)
